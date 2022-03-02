3a5868f7dbbb3e4b5a990adcf59a2ecd2f5b6df4
Natalia Sidorenko <sidorenkina91@gmail.com>
2022-03-02 20:10:22 +0700

    Урок 3

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from http import HTTPStatus
 
-# Create your tests here.
+from users.models import User
+
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url_registration = reverse('users:registration')
+        self.user_data = {
+            'first_name': 'Наталья', 'last_name': 'Сидоренко', 'username': 'Nata',
+            'email': '1@mail.ru', 'password1': '12345678q+', 'password2': '12345678q+'
+        }
+
+    def test_user_registration_GET(self):
+        response = self.client.get(self.url_registration)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_user_registration_POST(self):
+
+        response = self.client.post(self.url_registration, self.user_data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=self.user_data['username']).exists())
+
+    def test_user_registration_POST_already_exists(self):
+        data = {
+            'first_name': 'Наталья', 'last_name': 'Сидоренко', 'username': 'Nata',
+            'email': '1@mail.ru', 'password1': '12345678q_', 'password2': '12345678q_'
+        }
+
+        self.client.post(self.url_registration, self.user_data)
+        response = self.client.post(self.url_registration, data)
+
+        response_form = response.context['form']
+        self.assertFalse(response_form.is_valid())
+        self.assertEqual(response_form.errors['username'][0], 'Пользователь с таким именем уже существует.')
+        self.assertEqual(User.objects.count(), 1)
+
+
+
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+from django.contrib import messages
 
-# Create your views here.
+from users.forms import UserLoginForms, UserRegistrationForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForms(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForms()
+    context = {'title': 'Store - Авторизация', 'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались!')
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'title': 'Store - Регистрация', 'form': form}
+    return render(request, 'users/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
